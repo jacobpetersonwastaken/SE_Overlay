c8ec07544e496ea02481621e08cff3fe6a1da51d
OneCent01 <jmpenney22@gmail.com>
2023-04-30 23:42:41 -0700

    rt

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -4,4 +4,4 @@ export const MAV_USER_ID = '170166760';
 export const FATE_BOT_USER_ID = '904052148';
 export const APPLICATION_ID = '29gkmfk098vqw909sl73keg3r38bl2'
 
-export const ACCESS_TOKEN = 'r0ntkepo27kiwtwqwinbq7wanb52ed';
+export const ACCESS_TOKEN = 'ACCESS_TOKEN';
